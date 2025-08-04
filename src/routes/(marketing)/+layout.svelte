a16d276066977203508f1c3400f878f69184e611
Adrian <adunham95@gmail.com>
2025-08-04 18:41:13 -0400

    Updated marketing layout

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -2,6 +2,7 @@
 	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import { queryClient } from '$lib/API/queryClient.js';
 	let { children } = $props();
+	let mobileNabOpen = $state(false);
 </script>
 
 <QueryClientProvider client={queryClient}>
@@ -16,14 +17,23 @@
 					<span class="ml-2 text-2xl">Career Fingerprint</span>
 				</a>
 			</div>
-			<div class="flex lg:hidden">
+			<div class="flex md:hidden">
 				<button
 					type="button"
-					class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+					onclick={() => (mobileNabOpen = !mobileNabOpen)}
+					class="text-content-2 hover:bg-content-2 hover:text-content-3 focus:ring-primary relative inline-flex items-center justify-center rounded-md p-2 focus:ring-2 focus:outline-none focus:ring-inset"
+					aria-controls="mobile-menu"
+					aria-expanded="false"
 				>
-					<span class="sr-only">Open main menu</span>
+					<span class="absolute -inset-0.5"></span>
+					<span class="sr-only">Open menu</span>
+					<!--
+					Icon when menu is closed.
+					
+					Menu open: "hidden", Menu closed: "block"
+					-->
 					<svg
-						class="size-6"
+						class={`${mobileNabOpen ? 'hidden' : 'block'} size-6`}
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke-width="1.5"
@@ -37,76 +47,109 @@
 							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
 						/>
 					</svg>
+					<!--
+					Icon when menu is open.
+					
+					Menu open: "block", Menu closed: "hidden"
+            -->
+					<svg
+						class={`${mobileNabOpen ? 'block' : 'hidden'} size-6`}
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+						data-slot="icon"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+					</svg>
 				</button>
 			</div>
-			<div class="hidden lg:flex lg:gap-x-12">
-				<a href="/about" class="text-sm/6 font-semibold text-gray-900">About</a>
-			</div>
-			<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-				<a href="/get-started" class="mr-2 text-sm/6 font-semibold text-gray-900">Get started</a>
-				|
-				<a href="/dashboard" class="ml-2 text-sm/6 font-semibold text-gray-900"
+			<div class="hidden lg:flex lg:gap-x-12"></div>
+			<div class="hidden items-center md:flex md:flex-1 md:flex-row md:justify-end">
+				<a
+					href="https://careerfingerprint.app/get-started"
+					class="mr-2 text-sm/6 font-semibold text-gray-900">Get started</a
+				>
+				<div class=" h-[1.5rem] w-0.5 bg-gray-900"></div>
+				<a
+					href="https://careerfingerprint.app/dashboard"
+					class="ml-2 text-sm/6 font-semibold text-gray-900"
 					>Log in <span aria-hidden="true">&rarr;</span></a
 				>
 			</div>
 		</nav>
 		<!-- Mobile menu, show/hide based on menu open state. -->
-		<div class="hidden" role="dialog" aria-modal="true">
-			<!-- Background backdrop, show/hide based on slide-over state. -->
-			<div class="fixed inset-0 z-50"></div>
+		<div class={`lg:hidden  ${mobileNabOpen ? 'block' : 'hidden'}`}>
+			<!--
+          Mobile menu overlay, show/hide based on mobile menu state.
+  
+          Entering: "duration-150 ease-out"
+            From: "opacity-0"
+            To: "opacity-100"
+          Leaving: "duration-150 ease-in"
+            From: "opacity-100"
+            To: "opacity-0"
+        -->
+			<div class="fixed inset-0 z-20 bg-black/25" aria-hidden="true"></div>
+
+			<!--
+          Mobile menu, show/hide based on mobile menu state.
+  
+          Entering: "duration-150 ease-out"
+            From: "opacity-0 scale-95"
+            To: "opacity-100 scale-100"
+          Leaving: "duration-150 ease-in"
+            From: "opacity-100 scale-100"
+            To: "opacity-0 scale-95"
+        -->
 			<div
-				class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+				class="absolute inset-x-0 top-0 z-30 mx-auto w-full max-w-3xl origin-top transform p-2 transition"
 			>
-				<div class="flex items-center justify-between">
-					<a href="/" class="-m-1.5 p-1.5">
-						<span class="sr-only">Career Fingerprint</span>
-						<img class="h-8 w-auto" src="/logo-brand.svg" alt="Career Fingerprint Logo" />
-					</a>
-					<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-						<span class="sr-only">Close menu</span>
-						<svg
-							class="size-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
-							data-slot="icon"
-						>
-							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-						</svg>
-					</button>
-				</div>
-				<div class="mt-6 flow-root">
-					<div class="-my-6 divide-y divide-gray-500/10">
-						<div class="space-y-2 py-6">
-							<a
-								href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-								>Product</a
-							>
-							<a
-								href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-								>Features</a
-							>
-							<a
-								href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-								>Marketplace</a
-							>
-							<a
-								href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-								>Company</a
-							>
+				<div class="divide-y divide-gray-200 rounded-lg bg-white shadow-lg ring-1 ring-black/5">
+					<div class="pt-3 pb-2">
+						<div class="flex items-center justify-between px-4">
+							<div class="flex justify-center">
+								<img class="h-8 w-auto" src="/logo-brand.svg" alt="Career Fingerprint Logo" />
+								<span class="ml-2 text-2xl">Career Fingerprint</span>
+							</div>
+							<div class="-mr-2">
+								<button
+									type="button"
+									onclick={() => (mobileNabOpen = !mobileNabOpen)}
+									class="focus:ring-primaryfocus:outline-none relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:ring-2 focus:ring-inset"
+								>
+									<span class="absolute -inset-0.5"></span>
+									<span class="sr-only">Close menu</span>
+									<svg
+										class="size-6"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										aria-hidden="true"
+										data-slot="icon"
+									>
+										<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+									</svg>
+								</button>
+							</div>
 						</div>
-						<div class="py-6">
+						<div class="mt-3 space-y-1 px-2">
 							<a
-								href="#"
-								class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-								>Log in</a
+								href="https://careerfingerprint.app/get-started"
+								onclick={() => (mobileNabOpen = !mobileNabOpen)}
+								class="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
 							>
+								Get Started
+							</a>
+							<a
+								href="https://careerfingerprint.app/dashboard"
+								onclick={() => (mobileNabOpen = !mobileNabOpen)}
+								class="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-800"
+							>
+								Login
+							</a>
 						</div>
 					</div>
 				</div>
