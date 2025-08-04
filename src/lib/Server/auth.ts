c14371e7e7731999eb364c13a6ca8da2616e8349
Adrian <adunham95@gmail.com>
2025-08-04 13:57:02 -0400

    Added credentials

--- src/lib/Server/auth.ts
+++ src/lib/Server/auth.ts
@@ -3,6 +3,7 @@ import { PUBLIC_API_URL } from '$env/static/public';
 export async function validateAuthToken(token: string) {
 	try {
 		const res = await fetch(`${PUBLIC_API_URL}/auth/current-user`, {
+			credentials: 'include',
 			headers: {
 				Authorization: 'Bearer ' + token
 			}
