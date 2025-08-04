bdd213584c880152976655d7722aa0c6a415f71f
Adrian <adunham95@gmail.com>
2025-08-04 19:08:42 -0400

    Added pre release banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,5 +7,13 @@
 	let { children } = $props();
 </script>
 
+<div
+	class="bg-warning-500/50 sticky top-0 z-50 w-full items-center justify-center gap-x-6 px-6 py-2.5 sm:px-3.5"
+>
+	<p class="text-center text-sm/6 font-bold text-black">
+		This is pre-release software. Expect bugs and unfinshed elements
+	</p>
+</div>
+
 <ToastContainer />
 {@render children()}
