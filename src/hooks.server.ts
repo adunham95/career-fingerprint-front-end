dc06b73bc27787a5564dca87232555eac9e63873
Adrian <adunham95@gmail.com>
2025-08-06 09:46:27 -0400

    Added  Sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,15 @@
+import {sequence} from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import type { Handle } from '@sveltejs/kit';
 import * as auth from '$lib/Server/auth';
 
-export const handle: Handle = async ({ event, resolve }) => {
+Sentry.init({
+    dsn: "https://33aa3e5181ec848a1716968ca1bc34fd@o4509797399265280.ingest.us.sentry.io/4509797400576000",
+    tracesSampleRate: 1,
+    enableLogs: true
+})
+
+export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
 	const authToken = event.cookies.get('accessToken');
 	if (!authToken) {
 		event.locals.user = null;
@@ -19,4 +27,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.user = user;
 
 	return resolve(event);
-};
+});
+export const handleError = Sentry.handleErrorWithSentry();
