23cc7349073aa840c7740b1e9c24312a2d745edf
Adrian <adunham95@gmail.com>
2025-09-19 12:30:54 -0400

    Only allow active users

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -4,6 +4,7 @@ export const load = async (event) => {
 	console.log({ local: event.locals.user });
 	console.log(event.url.pathname);
 	if (!event.locals.user) redirect(302, `/login?redirect=${event.url.pathname}`);
+	if (event.locals.user.accountStatus !== 'active') redirect(302, '/login');
 
 	return { user: event.locals.user };
 };
