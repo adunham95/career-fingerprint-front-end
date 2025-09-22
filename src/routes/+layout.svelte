b7289827d4d305cc1bca91643b31b99c8d71ecd2
Adrian <adunham95@gmail.com>
2025-09-21 21:08:34 -0400

    Moved mix panel to client load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import { PUBLIC_MIXPANEL_TOKEN } from '$env/static/public';
+	import mixpanel from 'mixpanel-browser';
 	import ToastContainer from '$lib/Components/Toasts/ToastContainer.svelte';
 	import '../app.css';
 	import '../button-variants.css';
 	import '@tailwindplus/elements';
 
 	let { children } = $props();
+	mixpanel.init(PUBLIC_MIXPANEL_TOKEN, { debug: false, track_pageview: false });
 </script>
 
 <ToastContainer />
--- src/routes/+layout.ts
+++
@@ -1,4 +0,0 @@
-import { PUBLIC_MIXPANEL_TOKEN } from '$env/static/public';
-import mixpanel from 'mixpanel-browser';
-
-mixpanel.init(PUBLIC_MIXPANEL_TOKEN, { debug: false, track_pageview: false });
