179c829b3a2248d8daf5109c9c25b402adcb8891
Adrian <adunham95@gmail.com>
2025-10-09 19:50:12 -0400

    Added colors

--- src/lib/Components/DashboardActionButton.svelte
+++ src/lib/Components/DashboardActionButton.svelte
@@ -87,22 +87,8 @@
 		</div>
 	</a>
 {:else if demoLocked}
-	<div class={`${baseStyles} border-gray-600/30 bg-gray-600/10 p-2`}>
+	<div class={`${baseStyles} ${colorStyle()}  p-2`}>
 		<div class=" absolute top-1 right-1 flex justify-center rounded-lg border-2 bg-gray-400 p-1">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="size-4"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
-				/>
-			</svg>
 			<p class="mx-1 text-sm">Unavailable for demo</p>
 		</div>
 		<div class="opacity-30">
