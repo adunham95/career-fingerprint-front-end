74d61e2ac2935b4f0d54033b6375bc3fd0b60521
Adrian <adunham95@gmail.com>
2025-10-24 20:17:19 -0400

    Remove locals

--- src/routes/(app)/settings/membership/thank-you/+page.server.ts
+++ src/routes/(app)/settings/membership/thank-you/+page.server.ts
@@ -5,13 +5,15 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async (event) => {
 	if (!event.cookies.get('accessToken')) redirect(302, '/login');
-	if (!event.locals.user) redirect(302, '/login');
+	// if (!event.locals.user) redirect(302, '/login');
 	const sessionID = event.url.searchParams.get('session_id') || '';
 
 	try {
 		const api = createApiClient(event);
 		const validated = await validateSubscription(sessionID, api);
-		event.locals.user.subscription = validated;
+		if (event?.locals?.user?.subscription) {
+			event.locals.user.subscription = validated;
+		}
 		return { validated };
 	} catch (error) {
 		console.error(error);
