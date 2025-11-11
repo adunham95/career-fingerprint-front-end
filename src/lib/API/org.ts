9be76267600f6173ddf7384bc97f50119e6868d8
Adrian <adunham95@gmail.com>
2025-11-11 10:12:30 -0500

    Updated ability to create org, from client

--- src/lib/API/org.ts
+++ src/lib/API/org.ts
@@ -2,7 +2,7 @@ import { PUBLIC_API_URL } from '$env/static/public';
 import { createMutation, createQuery } from '@tanstack/svelte-query';
 import { queryClient } from './queryClient';
 import { createApiClient } from './apiClient';
-import type { OrgAdminUser } from '../../app';
+import type { OrgAdminUser, Organization } from '../../app';
 
 export async function registerOrg(newProfile: {
 	firstName: string;
@@ -45,24 +45,12 @@ export async function createOrg(newOrg: {
 	orgEmail: string;
 	orgLogo: string;
 	admin: number;
+	postalCode: string;
+	country: string;
 }) {
-	const url = `${PUBLIC_API_URL}/org`;
-
 	try {
-		const res = await fetch(url, {
-			method: 'POST',
-			credentials: 'include',
-			headers: {
-				'Content-Type': 'application/json' // Set content type to JSON
-			},
-			body: JSON.stringify(newOrg)
-		});
-		if (res.ok) {
-			return await res.json();
-		} else {
-			const message = await res.text();
-			throw new Error(`Failed to create org ${res.status} ${message}`);
-		}
+		const api = createApiClient();
+		await api.post('/org', newOrg);
 	} catch (error) {
 		console.log(error);
 		throw new Error(`Failed to register org`);
@@ -289,7 +277,18 @@ export async function getOrgDomains(orgID: string) {
 	}
 }
 
+export async function getMyOrgs() {
+	try {
+		const api = createApiClient();
+		return api.get<Organization[]>('/org/my');
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+}
+
 export const orgKeys = {
+	orgs: ['orgs'] as const,
 	org: (id: string) => ['org', id] as const,
 	orgAdmins: (id: string) => ['orgAdmins', id] as const,
 	orgUsers: (id: string, page = 1, pageSize = 20) => ['orgUsers', id, page, pageSize] as const,
@@ -303,6 +302,13 @@ export const useOrgUsersByPageQuery = (orgID: string, page: () => number, size =
 	return createQuery({
 		queryKey: orgKeys.orgUsers(orgID, page(), size),
 		queryFn: () => getOrgUsers(orgID, page(), size)
+	});
+};
+
+export const useMyOrgs = () => {
+	return createQuery({
+		queryKey: orgKeys.orgs,
+		queryFn: () => getMyOrgs()
 	});
 };
 
--- src/routes/(app)/settings/org/newOrg.svelte
+++ src/routes/(app)/settings/org/newOrg.svelte
@@ -6,13 +6,21 @@
 	import TextInput from '$lib/Components/FormElements/TextInput.svelte';
 	import TextInputWAddOns from '$lib/Components/FormElements/TextInputWAddOns.svelte';
 	import { toastStore } from '$lib/Components/Toasts/toast';
+	import Select from '$lib/Components/FormElements/Select.svelte';
 
 	const { userID, onSuccess }: { userID: number; onSuccess?: () => void } = $props();
 
 	let orgName = $state('');
-	let orgDomain = $state('');
 	let orgEmail = $state('');
 	let orgLogo = $state('');
+	let orgDomain = $state('');
+	let streetAddress = $state('');
+	let city = $state('');
+	let providence = $state('');
+	let zip = $state('');
+	let country = $state('US');
+
+	let errorMessage = $state<string | null>(null);
 
 	let createOrgMutation = useCreateOrg();
 
@@ -22,11 +30,19 @@
 		try {
 			await $createOrgMutation.mutateAsync({
 				orgName,
-				orgDomain,
 				orgEmail,
 				orgLogo,
+				orgDomain,
+				postalCode: zip,
+				country,
 				admin: userID
 			});
+			orgName = '';
+			orgEmail = '';
+			orgLogo = '';
+			orgDomain = '';
+			zip = '';
+			country = '';
 			toastStore.show({ message: 'New Org Created', type: 'success' });
 			onSuccess?.();
 		} catch (error) {}
@@ -36,29 +52,66 @@
 <TwoColumn title={'New Organization information'}>
 	<Card className="md:col-span-2">
 		<div class={`${formState === 'details' ? 'block' : 'hidden'}`}>
-			<div class="mt-4 grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-4">
-				<ImageUpload className="md:col-span-4" id="logo" label="Logo" bind:value={orgLogo} />
+			<div class="mt-4 grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-6">
+				<ImageUpload className="md:col-span-6" id="logo" label="Logo" bind:value={orgLogo} />
+				<TextInput bind:value={orgName} required id="name" label="Name" className="md:col-span-6" />
 				<TextInput
-					bind:value={orgName}
-					id="name"
-					label="Organization Name"
-					className="md:col-span-4"
+					required
+					id="org-email"
+					bind:value={orgEmail}
+					label="Billing Email"
+					className="md:col-span-3"
+					type="email"
 				/>
-
-				<TextInputWAddOns
+				<!-- <TextInputWAddOns
 					preInlineText="example@"
 					bind:value={orgDomain}
 					className="md:col-span-2"
 					id="org-domain"
 					label="Org Domain"
+				/> -->
+				<!-- Collect Billing Address -->
+				<TextInput
+					bind:value={streetAddress}
+					required
+					id="street-address"
+					label="Billing Street Address"
+					className="md:col-span-3"
 				/>
 				<TextInput
-					id="org-email"
-					bind:value={orgEmail}
-					label="Organization Email"
-					subLabel="Used for organization communication and billing"
+					bind:value={city}
+					required
+					id="address-city"
+					label="City"
 					className="md:col-span-2"
-					type="email"
+				/>
+				<TextInput
+					bind:value={providence}
+					required
+					id="address-state"
+					label="State/Providence"
+					className="md:col-span-1"
+				/>
+				<TextInput
+					bind:value={zip}
+					required
+					id="address-zip"
+					label="Zip/PostalCode"
+					className="md:col-span-1"
+				/>
+				<Select
+					bind:value={country}
+					className="md:col-span-2"
+					id="address-country"
+					label="County"
+					options={[
+						{ label: 'United States', id: 'US' },
+						{ label: 'Bahamas', id: 'BS' },
+						{ label: 'Canada', id: 'CA' },
+						{ label: 'Jamaica', id: 'JM' },
+						{ label: 'United Kingdom', id: 'GB' },
+						{ label: 'Virgin Islands, U.S.', id: 'VI' }
+					]}
 				/>
 			</div>
 		</div>
