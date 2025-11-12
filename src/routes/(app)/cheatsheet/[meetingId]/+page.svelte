31295e569a73d9dcaffbdc31ee228fca222d9395
Adrian <adunham95@gmail.com>
2025-11-12 16:13:49 -0500

    Added thank you modal

--- src/routes/(app)/cheatsheet/[meetingId]/+page.svelte
+++ src/routes/(app)/cheatsheet/[meetingId]/+page.svelte
@@ -6,11 +6,13 @@
 	import PageContainer from '$lib/Components/Containers/PageContainer.svelte';
 	import Label from '$lib/Components/FormElements/Label.svelte';
 	import TextArea from '$lib/Components/FormElements/TextArea.svelte';
+	import TextInput from '$lib/Components/FormElements/TextInput.svelte';
 	import NewMeetingForm from '$lib/Components/Forms/MeetingForm.svelte';
 	import NavPillButtons from '$lib/Components/Header/NavPillButtons.svelte';
 	import InfoBlock from '$lib/Components/InfoBlock.svelte';
 	import Loader from '$lib/Components/Loader.svelte';
 	import Drawer from '$lib/Components/Overlays/Drawer.svelte';
+	import Modal from '$lib/Components/Overlays/Modal.svelte';
 	import { toastStore } from '$lib/Components/Toasts/toast';
 	import { trackingStore } from '$lib/Stores/tracking.js';
 	import { formatDate } from '$lib/Utils/formatDate';
@@ -287,3 +289,41 @@
 		<Loader />
 	{/if}
 </Drawer>
+
+<Modal isOpen title="Add Thank You Note" size="lg">
+	<div class="grid grid-cols-2 gap-x-2">
+		<TextArea id="thank-you-note" label="Thank You Note" rows={4} />
+		<div>
+			<Label id="idx" label="Add Contacts" />
+			<div class="flex gap-x-1">
+				<TextInput id="firstName" placeholder="First Name" />
+				<TextInput id="email" placeholder="Email" />
+				<button class="btn btn-text--error btn-small">
+					<span class="sr-only"> Delete </span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-4"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+						/>
+					</svg>
+				</button>
+			</div>
+			<button
+				class="mt-2 w-full rounded-md border border-gray-400 p-2 text-center text-sm hover:bg-gray-400"
+				>Add Contact</button
+			>
+		</div>
+	</div>
+	{#snippet actions()}
+		<button class="btn btn-text--secondary">Do Not Send Thank You Note</button>
+		<button class="btn btn--primary">Send Note</button>
+	{/snippet}
+</Modal>
