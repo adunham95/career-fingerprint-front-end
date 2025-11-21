dc676c41850909946806b7479333dcabaac917af
Adrian Dunham <adunham95@gmail.com>
2025-11-21 13:25:21 -0500

    Added download button (#20)

--- src/routes/(app)/timeline/+page.svelte
+++ src/routes/(app)/timeline/+page.svelte
@@ -13,6 +13,9 @@
 	import Select from '$lib/Components/FormElements/Select.svelte';
 	import DateInputV2 from '$lib/Components/FormElements/DateInputV2.svelte';
 	import Loader from '$lib/Components/Loader.svelte';
+	import { PUBLIC_API_URL } from '$env/static/public';
+	import PremiumBadge from '$lib/Components/PremiumBadge.svelte';
+	import { useFeatureGate } from '$lib/Utils/featureGate.js';
 	const { data } = $props();
 
 	let jobPositionID = $state<string | null>(null);
@@ -61,8 +64,69 @@
 				onclick={() => {
 					window.print();
 					trackingStore.trackAction('Print Achievement Click');
-				}}>Print Timeline</button
-			>
+				}}
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="size-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"
+					/>
+				</svg>
+				<span class="sr-only">Print Timeline</span>
+			</button>
+			{#if useFeatureGate(1, data.user)}
+				<a
+					class="btn btn-text--primary relative mr-4 ml-2 flex justify-center"
+					target="_blank"
+					download
+					href={`${PUBLIC_API_URL}/achievement/my/pdf`}
+					onclick={() => trackingStore.trackAction('Download Cheatsheet')}
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+						/>
+					</svg>
+					<span class="sr-only">Download Timeline</span>
+
+					<PremiumBadge />
+				</a>
+			{:else}
+				<div class="btn btn-text--disabled relative mr-4 ml-2 cursor-not-allowed opacity-25">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+						/>
+					</svg>
+					<PremiumBadge />
+					<span class="sr-only">Download Timeline</span>
+				</div>
+			{/if}
+
 			<button
 				class="btn btn--primary"
 				onclick={() => {
