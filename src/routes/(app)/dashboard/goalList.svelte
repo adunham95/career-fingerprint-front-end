43fb008909b26639e039177d4c154b8d9cf44b43
Adrian Dunham <adunham95@gmail.com>
2025-11-30 12:01:05 -0500

    v20251129 (#25)
    * Updated weekly page to show goals
    
    * Changed to use goals based dashboard

--- src/routes/(app)/dashboard/goalList.svelte
+++ src/routes/(app)/dashboard/goalList.svelte
@@ -6,31 +6,29 @@
 	const myGoals = useGetMyGoals({ active: true, showProgress: true, limit: 5 });
 </script>
 
-<div class="bg-surface-100 rounded border-3 border-gray-200 p-4 md:col-span-2">
-	<h2 class="font-title pb-4 text-2xl">My Goals</h2>
-	{#if $myGoals.isLoading}
-		<div class="flex justify-center">
-			<Loader />
-		</div>
-	{:else if ($myGoals.data || [])?.length > 0}
-		<div class="divide-y divide-gray-200">
-			{#each $myGoals.data as goal}
-				<div class="px-4 py-4 sm:px-0">
-					<h3>
-						{goal.name}
-					</h3>
-					<p>Progress: {goal.progress * 100}%</p>
-					<div class="my-2 flex overflow-hidden rounded-full bg-gray-200">
-						<div
-							style={`width: ${goal.progress * 100}%`}
-							class="bg-primary-500 h-2 rounded-full"
-						></div>
-					</div>
-					<div class="mt-2 flex gap-2"></div>
+<h2 class="font-title pb-4 text-2xl">My Goals</h2>
+{#if $myGoals.isLoading}
+	<div class="flex justify-center">
+		<Loader />
+	</div>
+{:else if ($myGoals.data || [])?.length > 0}
+	<div class="divide-y divide-gray-200">
+		{#each $myGoals.data as goal}
+			<div class="px-4 py-4 sm:px-0">
+				<h3>
+					{goal.name}
+				</h3>
+				<p>Progress: {Math.trunc(goal.progress * 100)}%</p>
+				<div class="my-2 flex overflow-hidden rounded-full bg-gray-200">
+					<div
+						style={`width: ${goal.progress * 100}%`}
+						class="bg-primary-500 h-2 rounded-full"
+					></div>
 				</div>
-			{/each}
-		</div>
-	{:else}
-		<InfoBlock title="Goals" description="Your goals are empty. To add a goal click add goal" />
-	{/if}
-</div>
+				<div class="mt-2 flex gap-2"></div>
+			</div>
+		{/each}
+	</div>
+{:else}
+	<InfoBlock title="Goals" description="Your goals are empty. To add a goal click add goal" />
+{/if}
