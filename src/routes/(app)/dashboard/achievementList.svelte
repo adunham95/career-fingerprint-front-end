8d08cf81badd01d90c586710511a3b935c82872a
Adrian Dunham <adunham95@gmail.com>
2025-12-02 14:18:00 -0500

    v20251202 (#28)
    * Updated weekly page to show goals
    
    * Changed to use goals based dashboard
    
    * Removed goals feature (#26)
    
    * Removed double cards

--- src/routes/(app)/dashboard/achievementList.svelte
+++ src/routes/(app)/dashboard/achievementList.svelte
@@ -7,27 +7,25 @@
 	const previewAchievements = usePreviewAchievements();
 </script>
 
-<div class="bg-surface-100 rounded border-3 border-gray-200 p-4 md:col-span-2">
-	<h1 class="font-title pb-4 text-2xl">My Achievement Timeline</h1>
-	{#if ($previewAchievements.data || []).length > 0}
-		<Timeline
-			dates={$previewAchievements.data?.map((ach) => {
-				return {
-					title: 'Achievement',
-					date: ach.startDate ? new Date(ach.startDate) : new Date(),
-					description: ach.myContribution,
-					type: 'achievement'
-				};
-			}) || []}
-		/>
-	{:else if $previewAchievements.isLoading}
-		<div class="flex justify-center">
-			<Loader />
-		</div>
-	{:else}
-		<InfoBlock
-			title="Achievements"
-			description="Your achievement timeline is empty. To add a new achievement click Add Achievement"
-		/>
-	{/if}
-</div>
+<h1 class="font-title pb-4 text-2xl">My Achievement Timeline</h1>
+{#if ($previewAchievements.data || []).length > 0}
+	<Timeline
+		dates={$previewAchievements.data?.map((ach) => {
+			return {
+				title: 'Achievement',
+				date: ach.startDate ? new Date(ach.startDate) : new Date(),
+				description: ach.myContribution,
+				type: 'achievement'
+			};
+		}) || []}
+	/>
+{:else if $previewAchievements.isLoading}
+	<div class="flex justify-center">
+		<Loader />
+	</div>
+{:else}
+	<InfoBlock
+		title="Achievements"
+		description="Your achievement timeline is empty. To add a new achievement click Add Achievement"
+	/>
+{/if}
