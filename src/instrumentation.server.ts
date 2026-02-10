26c64825c38669a6939dfe11eaae1681a83e09d4
Adrian <adunham95@gmail.com>
2026-02-10 15:09:47 -0500

    Clean up

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -1,14 +1,12 @@
 import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 import * as Sentry from '@sentry/sveltekit';
 
-Sentry.init({
-	dsn: PUBLIC_SENTRY_DSN,
-
-	tracesSampleRate: 1.0,
-
-	// Enable logs to be sent to Sentry
-	enableLogs: true
-
-	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
-	// spotlight: import.meta.env.DEV,
-});
+if (process.env.NODE_ENV === 'production' && PUBLIC_SENTRY_DSN) {
+	Sentry.init({
+		dsn: PUBLIC_SENTRY_DSN,
+		tracesSampleRate: 0.1,
+		enableLogs: false
+		// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+		// spotlight: import.meta.env.DEV,
+	});
+}
